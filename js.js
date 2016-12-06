64c95403698f91ad9a2092b2d8242176e2154c69
Venduro <vng167@gmail.com>
2016-12-05 22:38:58 -0800

    refactored A1 and form submission

--- js.js
+++ js.js
@@ -1,3 +1,21 @@
-//test to make sure the javascript is linked up properly
+//Javascript will only run once the DOM has fully loaded.
+$( document ).ready(function() {
+  //variable created for the form
+  var $form = $('.form');
+  //section A1 radio buttons
+  // var $A1 = $('[name="A1"]');
+  // var $A1 = $("input[name='A1']:checked");
+  // var $B1 = $('#B1');
+  // var $C1 = $('#C1');
 
-//alert("js is link properly!");
+  $form.on('submit', function (e) {
+    e.preventDefault();
+
+    var $A1 = $("input[name='A1']:checked");
+    console.log($A1.val());
+    // var $li = $('<li>');
+    // var A1Value = $('<li>').html($A1.val());
+    //
+    // $li.append($A1Value);
+  });
+});
